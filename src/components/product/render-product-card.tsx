4e0cb5a4435930180e68eeb4b8a42261caadc8a6
Elvis Marçal <emarcal00@gmail.com>
2022-04-17 16:46:36 +0100

    enviar

--- src/components/product/render-product-card.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Neon = dynamic(() => import("@components/product/product-card/neon")); // grocery-two
-
-export default function renderProductCard(product: any, className = "") {
-  return <Neon product={product} className={className} />;
-
-}
